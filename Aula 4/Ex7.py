5ebbf4bbdd898800e838672c23fa3fbe2e1e7541
valéria <valleria.estudos@gmail.com>
2026-04-08 22:09:39 -0300

    ultimo exercicio

--- Aula 4/Ex7.py
+++ Aula 4/Ex7.py
@@ -3,15 +3,27 @@
 #Se opção 2, print o respectivo nome, idade e cidade da pessoa.
 #e opção 3, crie uma estrutura para sair do código.
 
+pessoa = []
+while True:
+    opcao = int(input("Qual opção deseja? 1-cadastrar, 2-listar ou 3-sair: "))
+    if opcao == 1:
+        nome = input("Digite o nome: ")
+        idade = int(input("Digite a idade: "))
+        cidade = input("Digite a cidade: ")
 
-opcao = int(input("Qual opção deseja? 1-cadastrar, 2-listar ou 3-sair: "))
-if opcao == 1:
-    nome = input("Digite o nome: ")
-    idade = int(input("Digite a idade: "))
-    cidade = input("Digite a cidade: ")
-    nome_idade_cidade = [nome,idade,cidade]
-elif opcao == 2:
-    print(nome_idade_cidade)
-else:
-    
-#incompleto
+        nome_idade_cidade = [nome,idade,cidade]
+        pessoa.append(nome_idade_cidade)
+
+        print("Cadastro realizado com sucesso!")
+
+    elif opcao == 2:
+        if len(pessoa) == 0:
+            print("Não existe cadastro")
+        else:
+            for i in pessoa:
+                print(f"Nome: {i[0]} Idade: {i[1]} Cidade:{i[2]}")
+    elif opcao == 3:
+        print("Saindo do sistema")
+        break
+    else:
+        print("Opção inválida.")
